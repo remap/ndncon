9bc44aff7bb13c1f5a3d5e5468ddc9bd2000eceb
peetonn <gpeetonn@gmail.com>
2014-09-09 11:02:50 -0700

    initial commit

---
+++ ndncon/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  NdnCon
+//
+//  Created by Peter Gusev on 9/8/14.
+//  Copyright (c) 2014 REMAP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ ndncon/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NdnCon
+//
+//  Created by Peter Gusev on 9/8/14.
+//  Copyright (c) 2014 REMAP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
