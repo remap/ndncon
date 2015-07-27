e6e3feec89655ac9d9ab76f8d7b54c2591ecbb68
peetonn <gpeetonn@gmail.com>
2015-07-27 00:33:35 -0700

    new UI interface files

---
+++ ndncon/NCBaseCapturer.h
@@ -0,0 +1,13 @@
+//
+//  NCBaseCapturer.h
+//  NdnCon
+//
+//  Created by Peter Gusev on 7/26/15.
+//  Copyright (c) 2015 REMAP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NCBaseCapturer : NSObject
+
+@end
